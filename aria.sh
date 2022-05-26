bad7d1604d9784946aadb1b4065b92b015d4d834
KentBDragon <106345852+KentBDragon@users.noreply.github.com>
2022-05-27 01:26:42 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n\n' ',')
 aria2c --enable-rpc --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M --bt-max-peers=0 \
